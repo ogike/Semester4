97a1bf053e9a01df498f627e05ff60cb43f292cf
ogike <77450926+ogike@users.noreply.github.com>
2022-02-10 18:15:59 +0100

    02.10

---
+++ Telekom/gyak1/if_statements.py
@@ -0,0 +1,12 @@
+
+team = [0,10,76]
+
+if 100 in team:
+    print ('Yes, 100 is in the team')
+elif 76 in team:
+    print ('100 is not in the team, but 76 is in it...')
+else:
+    print ('Both 100 and 76 are not in the team')
+
+for i in range (2,10,2):
+    print (i)
